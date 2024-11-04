0a3b007ef758ca747be6cef28438c51830c2e227
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2024-11-04 14:06:15 +0530

    Bump minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sustainhive.ecoconnect"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
